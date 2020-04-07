811ad1f6a2b00c6d54031eecb79137d69a4a5c84
Steve Mendes Reis <steve.mendesreis@he-arc.ch>
2020-04-07 14:12:51 +0200

    init

---
+++ PDiceGUI/src/test.java
@@ -0,0 +1,10 @@
+import javax.swing.JPanel;
+
+import ch.hearc.c_gui.tools.JFrameBaseLine;
+
+public class test {
+	public static void main(String[] args) {
+		new JFrameBaseLine(new JPanel());
+		System.out.println("test");
+	}
+}
